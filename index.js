08bd886557d3a1d6713ecd7172bc9c02fae5e751
taiphung143 <53302279+taiphung143@users.noreply.github.com>
2019-08-18 14:05:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setActivity(`Na là bot`, {
+client.user.setActivity(`Quản lí GáyZone` ,{
     type: "STREAMING",
-    url: "https://www.twitch.tv/namanhishere"})
+    url: "https://www.twitch.tv/taiphung"})
         .then(presence => console.log(`Your Status has been set to  ${presence.game ? presence.game.none : 'nome'}`))
         .catch(console.error);
 });
@@ -24,7 +24,7 @@ client.on('message', message => {
     message.reply(message.author.avatarURL);
   }
   
-    if (message.content.startsWith('pi.kick')) {
+    if (message.content.startsWith('!kick')) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
@@ -34,53 +34,30 @@ client.on('message', message => {
         if (!channel) return;
           channel.send(`Bạn Đã bị Kick Khỏi Gáy Zone, ${member}`);
         }).catch(err => {
-          message.reply('I was unable to kick the member');
+          message.reply('Tôi không có quyền để kick ${member}');
           console.error(err);
         });
       } else {
         message.reply('That user isn\'t in this guild!');
       }
     } else {
-      message.reply('You didn\'t mention the user to kick!');
+      message.reply('Bạn chưa ghi tên người bị kick');
     }
-  }
-  if (message.content === 'Nhỉ ?') {
-    message.reply('Đúng Rồi');
-  }
-  if (message.content === 'pi.ping') {
+  if (message.content === '!ping') {
     message.reply('Pong');
   }
-    if (message.content === 'pi.help') {
-    message.reply('Méo có lệnh j để mà help đâu');
-  }
-      if (message.content === 'pi.hi') {
+     if (message.content === '!hello') {
     message.reply('hi');
   }
-      if (message.content === 'pi.bye') {
+      if (message.content === '!bye') {
     message.reply('bye');
-  }
-      if (message.content === 'pi.goodmorning') {
-    message.reply('Chúc 1 ngày tốt lành');
   }
       if (message.content === 'pi.goodnight') {
     message.reply('Chúc Ngu Ngon');
   }
-      if (message.content === 'pi.luck') {
-    message.reply('Chúc May Mắn');
-  }
-      if (message.content === 'pi.test') {
-    message.reply('Bot Hoạt Động');
-  }
-        if (message.content === 'pi.tag') {
-    message.reply(`${client.user.tag}`);
-  }
-          if (message.content === 'pi.test1') {
-    message.reply(`Test`);
-  }
 
 
-
-  if (message.content.startsWith('pi.ban')) {
+  if (message.content.startsWith('!ban')) {
     const user = message.mentions.users.first();
     if (user) {
       const member = message.guild.member(user);
